51e6ea783a862350bf7195ca8a011fa692e08321
Hans van den Akker <hans@bizboard.nl>
2018-04-13 15:47:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     console.log('Logging: ' + Date.now());
-    return res.send('Hello World!')
+    return res.send('Hello Crazy8!')
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
